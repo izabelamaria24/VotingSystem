e475914443d0b38cca7d15aebfc8273c16a5f7ed
Iza <izabelajilavu@gmail.com>
2025-03-31 01:45:45 +0300

    party ballot

--- src/main/java/com/votingsystem/ballots/PartyBallot.java
+++ src/main/java/com/votingsystem/ballots/PartyBallot.java
@@ -1,4 +1,29 @@
 package main.java.com.votingsystem.ballots;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class PartyBallot {
+public class PartyBallot extends Ballot {
+    private List<String>parties;
+    private Map<String, Integer> votes;
+
+    public PartyBallot(List<String> parties) {
+        super(BallotType.PARTY_BALLOT);
+        this.parties = parties;
+        this.votes = new HashMap<>();
+
+        for (String party : parties) {
+            votes.put(party, 0);
+        }
+    }
+
+    @Override
+    public void castVote(String voteOption) {
+        if (votes.containsKey(voteOption)) {
+            votes.put(voteOption, votes.get(voteOption) + 1);
+            System.out.println("Vote casted for party: " + voteOption);
+        } else {
+            System.out.println("Invalid party!");
+        }
+    }
 }
